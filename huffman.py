a444bdc6ed7bf0cff850f0b96c9873bcce99f2a9
Axel Hippolite <54190267+AxelHippolite@users.noreply.github.com>
2022-04-27 13:29:15 +0200

    Update huffman.py

--- huffman.py
+++ huffman.py
@@ -1,6 +1,11 @@
 from binary_tree import BTree
 
 def create_tree(nodes):
+    """
+    input: list of nodes
+    output: BTree/nodes
+    returns the constructed huffman tree
+    """
     while len(nodes) != 1:
         new_value = nodes[0].value + nodes[1].value
         new_bt = BTree(new_value, None, nodes[0], nodes[1])
@@ -10,6 +15,11 @@ def create_tree(nodes):
     return nodes[0]
 
 def code_tree(tree, code=''):
+    """
+    input: root of the BTree/BTree
+    output: dictionnary
+    returns a dictionary containing each character and its code
+    """
     if tree.isLeaf():
         return {tree.label: code}
     (left, right) = (tree.left_ch, tree.right_ch)
@@ -19,10 +29,20 @@ def code_tree(tree, code=''):
     return d
 
 def encode(dic, text):
+    """
+    input: dictionnary, original text
+    output: string
+    return the code representing the compressed file in binary form (bit string)
+    """
     code = ''
     for i in text:
         code += dic[i]
     return code
 
 def average_bits(dic):
+    """
+    input: dictionnary
+    output: integer
+    returns the average length in bits of the character codes
+    """
     return sum([len(x) for x in list(dic.values())])/len([len(x) for x in list(dic.values())])
