2053a74183bc2ee10fd1bf08862e014e134da6fb
Axel Hippolite <54190267+AxelHippolite@users.noreply.github.com>
2022-04-27 13:02:10 +0200

    Update binary_tree.py

--- binary_tree.py
+++ binary_tree.py
@@ -1,11 +1,21 @@
 class BTree:
     def __init__(self, value, label=None, left_ch=None, right_ch=None):
+        """
+        input: value, label, left_ch, right_ch
+        output: nothing
+        constructor of the class
+        """
         self.label = label
         self.value = value
         self.left_ch = left_ch
         self.right_ch = right_ch
 
     def isLeaf(self):
+        """
+        input: object BTree (self)
+        output: boolean
+        return true if the object passed in parameter is a leaf false otherwise
+        """
         return self.left_ch == None and self.right_ch == None
     
         
