f883cb0dd9a3c3bb66963c21a19be8059ea168a1
Axel Hippolite <54190267+AxelHippolite@users.noreply.github.com>
2022-04-27 13:20:11 +0200

    Update occurency.py

--- occurency.py
+++ occurency.py
@@ -1,4 +1,9 @@
 def finder(text):
+    """
+    input: text to compress (string)
+    output: list
+    return a list of tuples where each tuple contains a character and its number of occurrence in the text
+    """
     freq = []
     for i in range(256):
         if text.count(chr(i)) > 0:
@@ -6,6 +11,11 @@ def finder(text):
     return freq
 
 def cleaner(freq):
+    """
+    input: list
+    output: list
+    returns the same list as the finder() function but ordered by increasing frequency
+    """
     return freq.sort(key = lambda x : x[0])
     
             
