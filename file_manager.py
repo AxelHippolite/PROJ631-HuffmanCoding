0b7daf511055f6b98636242f8fb5bc6352464f1e
Axel Hippolite <54190267+AxelHippolite@users.noreply.github.com>
2022-04-27 13:14:45 +0200

    Update file_manager.py

--- file_manager.py
+++ file_manager.py
@@ -1,6 +1,11 @@
 import os
 
 def freq_writer(freq, name):
+    """
+    input: list of the number of occurrences, name of the original file
+    output: nothing
+    generates the text file that will contain the alphabet of the original text 
+    """
     file = open("results/" + name[:-4] + "_freq.txt", "w")
     file.write(str(len(freq)) + '\n')
     for occ in freq:
@@ -11,9 +16,19 @@ def freq_writer(freq, name):
     file.close()
 
 def bin_writer(encoding, name):
+    """
+    input: binary encoding of compressed text, name of the original file
+    output: nothing
+    generates the binary file that will contain the compressed text
+    """
     file = open("results/" + name[:-4] + "_comp.bin", "wb")
     file.write(encoding)
     file.close()
 
 def get_size(path):
+    """
+    input: path of the fil
+    output: float
+    return the size(in bytes) of the file
+    """
     return os.path.getsize(path)
